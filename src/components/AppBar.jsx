498a140407f878ce876a9630d9ac9f9a85386ea1
Yusif Salam-zade <yusalam@gmail.com>
2020-09-15 22:39:48 +0300

    10.4 cont.

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
-import Constants from 'expo-constants';
 import Text from './Text';
 
 const styles = StyleSheet.create({
   container: {
-    // paddingTop: Constants.statusBarHeight,
     marginBottom: 10,
     display: "flex",
   },
@@ -14,7 +12,6 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
     opacity: 10,
-    // flexGrow: 0,
     height: 70,
     paddingTop: 35
   }
