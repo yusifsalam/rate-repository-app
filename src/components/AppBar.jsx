85b6608babab213bd792c2f9aa70f59e38f21249
Yusif Salam-zade <yusalam@gmail.com>
2020-09-16 16:47:05 +0300

    10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { Link } from "react-router-native";
 import Text from "./Text";
 
 const styles = StyleSheet.create({
   container: {
     display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#24292e",
+    paddingTop: 40,
   },
   appBap: {
-    backgroundColor: "#24292e",
     color: "white",
     fontWeight: "bold",
     opacity: 10,
-    height: 70,
-    paddingTop: 35,
+    margin: 10,
   },
-  // ...
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <TouchableWithoutFeedback>
+      <Link to="/" component={TouchableWithoutFeedback}>
         <Text style={styles.appBap}> Repositories</Text>
-      </TouchableWithoutFeedback>
+      </Link>
+      <Link to="/sign-in" component={TouchableWithoutFeedback}>
+        <Text style={styles.appBap}>Sign in</Text>
+      </Link>
     </View>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { Route, Switch, Redirect } from "react-router-native";
+
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar";
+import SignIn from "./SignIn";
 
 const styles = StyleSheet.create({
   container: {
@@ -15,7 +18,15 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Switch>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Route path="/sign-in" exact>
+          <SignIn />
+        </Route>
+        {/* <Redirect to="/" /> */}
+      </Switch>
     </View>
   );
 };
