ac2c9b61ef5ab23927d90998cf32f8f54b1b75b9
Yusif Salam-zade <yusalam@gmail.com>
2020-09-17 12:07:13 +0300

    10.10

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text as NativeText, StyleSheet } from "react-native";
+import { Text as NativeText, StyleSheet, Platform } from "react-native";
 
 import theme from "../theme";
 
@@ -7,7 +7,11 @@ const styles = StyleSheet.create({
   text: {
     color: theme.colors.textPrimary,
     fontSize: theme.fontSizes.body,
-    fontFamily: theme.fonts.main,
+    fontFamily: Platform.select({
+      android: "Roboto",
+      ios: "Arial",
+      default: theme.fonts.main,
+    }),
     fontWeight: theme.fontWeights.normal,
   },
   colorTextSecondary: {
