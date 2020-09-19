2f099fc3e41185cb639ab335a9ce7baf4dd9cc78
Yusif Salam-zade <yusalam@gmail.com>
2020-09-19 11:52:25 +0300

    10.11

---
+++ src/graphql/queries.js
@@ -0,0 +1,21 @@
+import { gql } from "apollo-boost";
+
+export const GET_REPOSITORIES = gql`
+  query {
+    repositories {
+      edges {
+        node {
+          id
+          fullName
+          description
+          language
+          stargazersCount
+          forksCount
+          reviewCount
+          ratingAverage
+          ownerAvatarUrl
+        }
+      }
+    }
+  }
+`;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,24 +1,15 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+
+import { GET_REPOSITORIES } from "../graphql/queries";
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
+  const edgesExist = data && data.repositories && data.repositories.edges;
+  const repositories = edgesExist ? data.repositories : null;
 
-  const fetchRepositories = async () => {
-    setLoading(true);
-
-    const response = await fetch("http://192.168.1.7:5000/api/repositories");
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { data, error, loading, repositories };
 };
 
 export default useRepositories;
