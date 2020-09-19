d11c99d0bfa67c2bb00bf7bc3c87340142958313
Yusif Salam-zade <yusalam@gmail.com>
2020-09-19 12:19:33 +0300

    10.12

---
+++ app.config.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+
+export default {
+  name: "rate-repository-app",
+  slug: "rate-repository-app",
+  version: "1.0.0",
+  entryPoint: "./src/App.js",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  extra: {
+    env: process.env.ENV,
+    apolloUri: process.env.APOLLO_URI,
+  },
+};
